c952993aff9d06bef61cc6e34e8eea4fc8319e82
Jane Chu <7559015+janechu@users.noreply.github.com>
2021-09-14 17:19:47 -0700

    Add tooling packages from the FAST repository

---
+++ packages/fast-tooling/.eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+    extends: ["../../.eslintrc"],
+    rules: {
+        "@typescript-eslint/class-name-casing": "off",
+    },
+};
---
+++ packages/fast-tooling/src/index.ts
@@ -0,0 +1,5 @@
+export * from "./schemas";
+export * from "./data-utilities";
+export * from "./message-system";
+export * from "./message-system-service";
+export * from "./web-components";
